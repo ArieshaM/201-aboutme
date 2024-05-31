200b168f7cb75bb2e600fea55e7f34a862af85c6
Ariesha Murray <murrayariesha@gmail.com>
2024-05-31 09:48:05 -0500

    made changes to files

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+
+let person = prompt('Please enter your name', '');
+
+if (person !== null) prompt ('Hello ' + person + '! How are you today?');
+
+let place = prompt(' Where are you located', '');
+
+if (place !== null) prompt ( 'Is ' + place + ' Fun?');
+
+let food = prompt ( 'Do you have a favorite food?', 'What is it?');
+
+if (food !== null) prompt ( 'Great ' + food + ' sounds delicious!');
+
+let drink = prompt ('Do you have a favorite drink?', 'What is it?');
+
+if (drink !== null) prompt ( 'I like ' + drink + ' too!');
+
+let favColor = prompt (' Is my favorite color green?', '');
+
+if (favColor !== 'Yes'); {
+  alert ('True');
+}
+
