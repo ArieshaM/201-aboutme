ae65e172fec1a0040c0d7295206f5ee81301b9ee
Ariesha Murray <murrayariesha@gmail.com>
2024-06-24 11:46:13 -0500

    made changes to js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
 //*eslintrc.json
 
-let text = "yes!";
-let True = text.toUpperCase();
+//let text = "yes!";
+//let True = text.toUpperCase();
 
 let person = prompt('Please enter your name', '');
 
 if (person !== null) prompt ('Hello ' + person + '! How are you today?');
 
 let code = prompt(' Do I love to Code', '');{
-if ('code.toLowerCase() does user input match yes or y') alert 'true'
-elseif ('code.toLowerCase() does user input match or no or n')
-alert 'false'
+  if (code.toLowerCase('yes or y')) alert('True'); else
+    alert ('False');
 }
 
 let parent = prompt( 'Am I Parent?', ''); {
-if ('yes') alert ('True');
+  if (parent.toLowerCase('yes or y')) alert ('True'); else
+    alert ('False');
 }
 
 let food = prompt( 'Are veggies my favorite food?', ''); {
-if ('yes') alert ('True');
+  if (food.toLowerCase('yes or y')) alert ('True'); else
+    alert ('False');
 }
 
 let Jesus = prompt ('Am I a grateful believer of Jesus Christ?', ''); {
-if ('yes') alert ('True!');
+  if (Jesus.toLowerCase('yes or y')) alert ('True!'); else
+    alert ('False');
 }
 
 let travel = prompt (' Do I like to travel?', ''); {
-if ('yes') alert ('True!');
+  if (travel.toLowerCase('yes or y')) alert ('True!'); else
+    alert ('False');
 }
 
-
-
-   if code.toLowerCase() strictly equals yes/y, or no/n, then return the desired response
