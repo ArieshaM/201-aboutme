0d847c0ebca41944ccc1cd3b2312acefd0e69f75
Ariesha Murray <murrayariesha@gmail.com>
2024-06-24 10:53:22 -0500

    made changes to js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,9 @@ let person = prompt('Please enter your name', '');
 if (person !== null) prompt ('Hello ' + person + '! How are you today?');
 
 let code = prompt(' Do I love to Code', '');{
-if ('yes') alert ('True');
+if ('code.toLowerCase() does user input match yes or y') alert 'true'
+elseif ('code.toLowerCase() does user input match or no or n')
+alert 'false'
 }
 
 let parent = prompt( 'Am I Parent?', ''); {
@@ -26,3 +28,7 @@ if ('yes') alert ('True!');
 let travel = prompt (' Do I like to travel?', ''); {
 if ('yes') alert ('True!');
 }
+
+
+
+   if code.toLowerCase() strictly equals yes/y, or no/n, then return the desired response
